01edd256311faa187edc9c7951a5d413f88eae33
JianqinWang <jianqin.wang@scality.com>
2018-04-02 16:46:41 -0700

    rename

--- bin/ingestionQueuePopulator.js
+++ bin/ingestionQueuePopulator.js
@@ -10,39 +10,39 @@ const extConfigs = config.extensions;
 const qpConfig = config.queuePopulator;
 const mConfig = config.metrics;
 const rConfig = config.redis;
-const QueuePopulator = require('../lib/queuePopulator/IngestionProducer');
+const IngestionProducer = require('../lib/queuePopulator/IngestionProducer');
 const queueBatch = require('./utils').queueBatch;
 
-const log = new werelogs.Logger('Backbeat:QueuePopulator');
+const log = new werelogs.Logger('Backbeat:IngestionProducer');
 
 werelogs.configure({ level: config.log.logLevel,
     dump: config.log.dumpLevel });
 
-const queuePopulator = new QueuePopulator(zkConfig, kafkaConfig,
+const ingestionProducer = new IngestionProducer(zkConfig, kafkaConfig,
     qpConfig, mConfig,
     rConfig, extConfigs);
 
 async.waterfall([
-    done => queuePopulator.open(done),
-    done => queuePopulator.getBuckets(done),
-    (bucketList, done) => queuePopulator.getBucketMd(bucketList, done),
-    (bucketList, done) => queuePopulator.getBucketObjects(bucketList, done),
+    done => ingestionProducer.open(done),
+    done => ingestionProducer.getBuckets(done),
+    (bucketList, done) => ingestionProducer.getBucketMd(bucketList, done),
+    (bucketList, done) => ingestionProducer.getBucketObjects(bucketList, done),
     (bucketList, done) => {
-        queuePopulator.getBucketObjectsMetadata(bucketList, done);
+        ingestionProducer.getBucketObjectsMetadata(bucketList, done);
     },
     done => {
         const taskState = {
             batchInProgress: false,
         };
         schedule.scheduleJob(qpConfig.cronRule, () => {
-            queueBatch(queuePopulator, taskState, qpConfig, log);
+            queueBatch(ingestionProducer, taskState, qpConfig, log);
         });
         done();
     },
 ], err => {
     if (err) {
         log.error('error during queue populator initialization', {
-            method: 'QueuePopulator::task',
+            method: 'IngestionPopulator::task',
             error: err,
         });
         process.exit(1);
@@ -51,7 +51,7 @@ async.waterfall([
 
 process.on('SIGTERM', () => {
     log.info('received SIGTERM, exiting');
-    queuePopulator.close(() => {
+    ingestionProducer.close(() => {
         process.exit(0);
     });
 });
