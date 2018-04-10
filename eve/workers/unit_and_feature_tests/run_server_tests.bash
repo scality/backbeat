85c993298c11bb39e70e9b01cb6cc51a35ea443c
philipyoo <philip.yoo@scality.com>
2018-04-10 13:59:43 -0700

    fix: run metrics api tests in eve

---
+++ eve/workers/unit_and_feature_tests/run_server_tests.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+killandsleep () {
+  kill -9 $(lsof -t -i:$1) || true
+  sleep 10
+}
+
+# run backbeat server
+TEST_SWITCH=1 npm start & bash tests/utils/wait_for_local_port.bash 8900 40 && npm run ft_server_test
+
+killandsleep 8900
+
+exit $?
