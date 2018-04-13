3d2e180ff9760e5bb60e2148434dd2c61b06afdf
JianqinWang <jianqin.wang@scality.com>
2018-04-13 10:44:05 -0700

    ft: update config for ingestionProducer
    Co-authored-by: Lauren Spiegel <lhspiegel@gmail.com>

--- conf/config.joi.js
+++ conf/config.joi.js
@@ -6,6 +6,8 @@ const { hostPortJoi, logJoi } = require('../lib/config/configItems.joi.js');
 const transportJoi = joi.alternatives().try('http', 'https')
     .default('http');
 
+const logSourcesJoi = joi.string().valid('bucketd', 'mongo', 'ingestion');
+
 const joiSchema = {
     zookeeper: {
         connectionString: joi.string().required(),
@@ -35,13 +37,24 @@ const joiSchema = {
         cronRule: joi.string().required(),
         batchMaxRead: joi.number().default(10000),
         zookeeperPath: joi.string().required(),
-        logSource: joi.alternatives().try('bucketd', 'dmd').required(),
+        logSource: joi.alternatives().try(logSourcesJoi).required(),
         bucketd: hostPortJoi
             .when('logSource', { is: 'bucketd', then: joi.required() }),
         dmd: hostPortJoi.keys({
             logName: joi.string().default('s3-recordlog'),
         }).when('logSource', { is: 'dmd', then: joi.required() }),
+        mongo: joi.object({
+            replicaSetHosts: joi.string().default('localhost:27017'),
+            logName: joi.string().default('s3-recordlog'),
+            writeConcern: joi.string().default('majority'),
+            replicaSet: joi.string().default(''),
+            readPreference: joi.string().default('primary'),
+            database: joi.string().default('metadata'),
+        }).when('logSource', { is: 'mongo', then: joi.required() }),
+        ingestion: joi.array(),
     },
+    validLogSources: joi.array(),
+    ingestion: joi.object(),
     log: logJoi,
     extensions: joi.object(),
     metrics: {
