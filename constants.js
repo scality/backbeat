545d5dbd7b859d2054c94b1d5971e0e614e96605
JianqinWang <jianqin.wang@scality.com>
2018-04-13 13:42:40 -0700

    additional addition to config

--- constants.js
+++ constants.js
@@ -4,7 +4,6 @@ const constants = {
     usersBucket: 'users..bucket',
     metastoreBucket: 'metastore',
     oldUsersBucket: 'namespaceusersbucket',
-    raftSessionId: [1, 2, 3, 4, 5, 6, 7, 8],
 };
 
 module.exports = constants;
