8c8c7c770ac7792b1640fa9bd47b6087a7e1f982
Nicolas Humbert <nicolas.humbert@live.fr>
2018-04-17 11:14:46 -0700

    FX: No need to filter data since we are accepting multiple sites

--- extensions/replication/queueProcessor/QueueProcessor.js
+++ extensions/replication/queueProcessor/QueueProcessor.js
@@ -284,13 +284,7 @@ class QueueProcessor extends EventEmitter {
             });
             this._consumer.on('metrics', data => {
                 // i.e. data = { my-site: { ops: 1, bytes: 124 } }
-                const filteredData = Object.keys(data).filter(key =>
-                    key === this.site).reduce((store, k) => {
-                        // eslint-disable-next-line no-param-reassign
-                        store[k] = data[this.site];
-                        return store;
-                    }, {});
-                this._mProducer.publishMetrics(filteredData,
+                this._mProducer.publishMetrics(data,
                     metricsTypeProcessed, metricsExtension, err => {
                         this.logger.trace('error occurred in publishing ' +
                             'metrics', {
