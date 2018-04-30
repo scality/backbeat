6eb174d516b97cf0bfe577fde6744163e0a45879
philipyoo <philip.yoo@scality.com>
2018-04-30 11:39:19 -0700

    bf: instantiate a QueueProcessor per site

--- extensions/replication/queueProcessor/task.js
+++ extensions/replication/queueProcessor/task.js
@@ -1,5 +1,7 @@
 'use strict'; // eslint-disable-line
+
 const werelogs = require('werelogs');
+
 const QueueProcessor = require('./QueueProcessor');
 const config = require('../../../conf/Config');
 const { initManagement } = require('../../../lib/management');
@@ -16,6 +18,8 @@ const mConfig = config.metrics;
 const log = new werelogs.Logger('Backbeat:QueueProcessor:task');
 werelogs.configure({ level: config.log.logLevel,
     dump: config.log.dumpLevel });
+
+const activeQProcessors = {};
 
 const metricsProducer = new MetricsProducer(kafkaConfig, mConfig);
 metricsProducer.setupProducer(err => {
@@ -47,11 +51,35 @@ metricsProducer.setupProducer(err => {
 
             config.on('bootstrap-list-update', () => {
                 destConfig.bootstrapList = config.getBootstrapList();
+
+                const activeSites = Object.keys(activeQProcessors);
+                const updatedSites = destConfig.bootstrapList.map(i => i.site);
+                const allSites = [...new Set(activeSites.concat(updatedSites))];
+
+                allSites.forEach(site => {
+                    if (updatedSites.includes(site)) {
+                        if (!activeSites.includes(site)) {
+                            activeQProcessors[site] = new QueueProcessor(
+                                zkConfig, kafkaConfig, sourceConfig, destConfig,
+                                repConfig, metricsProducer, site);
+                            activeQProcessors[site].start();
+                        }
+                    } else {
+                        // this site is no longer in bootstrapList
+                        activeQProcessors[site].stop(() => {});
+                        delete activeQProcessors[site];
+                    }
+                });
             });
 
-            const queueProcessor = new QueueProcessor(zkConfig, kafkaConfig,
-                sourceConfig, destConfig, repConfig, metricsProducer);
-            queueProcessor.start();
+            // Start QueueProcessor for each site
+            const siteNames = bootstrapList.map(i => i.site);
+            siteNames.forEach(site => {
+                activeQProcessors[site] = new QueueProcessor(zkConfig,
+                    kafkaConfig, sourceConfig, destConfig, repConfig,
+                    metricsProducer, site);
+                activeQProcessors[site].start();
+            });
         });
     }
     return initAndStart();
