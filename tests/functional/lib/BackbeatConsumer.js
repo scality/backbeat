280c1bd61e74a5640ec737928bce0867e790c4cd
Jonathan Gramain <jonathan.gramain@scality.com>
2018-07-30 18:28:26 -0700

    bf: ZENKO-860 replace replication batching by continuous task flow
    Keep consuming kafka messages as soon as new workers become available
    in the queue, rather than relying on the 'drain' callback from
    async.queue.
    
    This prevents being stuck from consuming new messages in case one of
    the tasks takes a long time to finish (e.g. if retrying on a failing
    cloud).
    
    Change the way we manage Kafka consumer offsets: instead of disabling
    auto-commit by default, always enable it and disable automatic offset
    store of consumed messages. This allows to set the offset based on the
    processed messages rather than the consumed messages, relying on a
    helper class OffsetLedger to maintain knowledge of the highest offset
    that can be committed, i.e. that is a higher-bound of already
    processed message offsets.

---
+++ tests/functional/lib/BackbeatConsumer.js
@@ -0,0 +1,136 @@
+const assert = require('assert');
+const async = require('async');
+const BackbeatProducer = require('../../../lib/BackbeatProducer');
+const BackbeatConsumer = require('../../../lib/BackbeatConsumer');
+const zookeeperConf = { connectionString: 'localhost:2181' };
+const kafkaConf = { hosts: 'localhost:9092' };
+
+describe('BackbeatConsumer concurrency tests', () => {
+    const topicConc = 'backbeat-consumer-spec-conc-1000';
+    const groupIdConc = `replication-group-conc-${Math.random()}`;
+    let producer;
+    let consumer;
+    let consumedMessages = [];
+
+    function queueProcessor(message, cb) {
+        if (message.value.toString() !== 'taskStuck') {
+            consumedMessages.push(message.value);
+            process.nextTick(cb);
+        }
+    }
+    before(function before(done) {
+        this.timeout(60000);
+
+        producer = new BackbeatProducer({
+            kafka: kafkaConf,
+            topic: topicConc,
+            pollIntervalMs: 100,
+        });
+        consumer = new BackbeatConsumer({
+            zookeeper: zookeeperConf,
+            kafka: kafkaConf, groupId: groupIdConc, topic: topicConc,
+            queueProcessor,
+            concurrency: 10,
+            bootstrap: true,
+        });
+        async.parallel([
+            innerDone => producer.on('ready', innerDone),
+            innerDone => consumer.on('ready', innerDone),
+        ], done);
+    });
+    afterEach(() => {
+        consumedMessages = [];
+        consumer.removeAllListeners('consumed');
+    });
+    after(function after(done) {
+        this.timeout(10000);
+        async.parallel([
+            innerDone => producer.close(innerDone),
+            innerDone => consumer.close(innerDone),
+        ], done);
+    });
+
+    it('should be able to process 1000 messages with concurrency', done => {
+        const boatloadOfMessages = [];
+        for (let i = 0; i < 1000; ++i) {
+            boatloadOfMessages.push({
+                key: `message-${i}`,
+                message: `{"message_index":"${i}"}`,
+            });
+        }
+        async.series([
+            next => {
+                setTimeout(() => producer.send(boatloadOfMessages, err => {
+                    assert.ifError(err);
+                }), 1000);
+                let totalConsumed = 0;
+                consumer.subscribe();
+                consumer.on('consumed', messagesConsumed => {
+                    totalConsumed += messagesConsumed;
+                    assert(totalConsumed <= boatloadOfMessages.length);
+                    if (totalConsumed === boatloadOfMessages.length) {
+                        next();
+                    }
+                });
+            },
+            next => {
+                // looping to ease reporting when test fails
+                // (otherwise node gets stuck for ages during diff
+                // generation with an assert.deepStrictEqual() on
+                // whole message arrays)
+                assert.strictEqual(consumedMessages.length,
+                                   boatloadOfMessages.length);
+                for (let i = 0; i < consumedMessages.length; ++i) {
+                    assert.deepStrictEqual(consumedMessages[i].toString(),
+                                           boatloadOfMessages[i].message);
+                }
+                next();
+            },
+        ], done);
+    }).timeout(30000);
+
+    it('should not prevent progress with concurrency if one task is stuck',
+    done => {
+        const boatloadOfMessages = [];
+        const stuckIndex = 500;
+        for (let i = 0; i < 1000; ++i) {
+            boatloadOfMessages.push({
+                key: `message-${i}`,
+                message: i === stuckIndex ?
+                    'taskStuck' : `{"message_index":"${i}"}`,
+            });
+        }
+        async.series([
+            next => {
+                setTimeout(() => producer.send(boatloadOfMessages, err => {
+                    assert.ifError(err);
+                }), 1000);
+                let totalConsumed = 0;
+                consumer.subscribe();
+                consumer.on('consumed', messagesConsumed => {
+                    totalConsumed += messagesConsumed;
+                    assert(totalConsumed <= boatloadOfMessages.length);
+                    if (totalConsumed === boatloadOfMessages.length) {
+                        next();
+                    }
+                });
+            },
+            next => {
+                // looping to ease reporting when test fails
+                // (otherwise node gets stuck for ages during diff
+                // generation with an assert.deepStrictEqual() on
+                // whole message arrays)
+                assert.strictEqual(consumedMessages.length,
+                                   boatloadOfMessages.length - 1);
+                for (let i = 0; i < consumedMessages.length; ++i) {
+                    assert.deepStrictEqual(
+                        consumedMessages[i].toString(),
+                        i < stuckIndex ?
+                            boatloadOfMessages[i].message :
+                            boatloadOfMessages[i + 1].message);
+                }
+                next();
+            },
+        ], done);
+    }).timeout(30000);
+});
---
+++ tests/unit/OffsetLedger.spec.js
@@ -0,0 +1,167 @@
+const assert = require('assert');
+
+const OffsetLedger = require('../../lib/OffsetLedger');
+
+describe('OffsetLedger', () => {
+    it('should get no committable offset if no offset registered yet', () => {
+        const ledger = new OffsetLedger();
+
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), null);
+    });
+
+    it('should get correct offset with one consumed message', () => {
+        const ledger = new OffsetLedger();
+
+        ledger.onOffsetConsumed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+
+        const committableOffset = ledger.onOffsetProcessed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 43);
+        assert.strictEqual(committableOffset, 43);
+    });
+
+    it('should get correct offset with two messages processed in-order', () => {
+        const ledger = new OffsetLedger();
+
+        ledger.onOffsetConsumed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        ledger.onOffsetConsumed('topic', 0, 43);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        ledger.onOffsetProcessed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 43);
+        const committableOffset = ledger.onOffsetProcessed('topic', 0, 43);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 44);
+        assert.strictEqual(committableOffset, 44);
+    });
+
+    it('should get correct offset with two messages processed out-of-order',
+    () => {
+        const ledger = new OffsetLedger();
+
+        ledger.onOffsetConsumed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        ledger.onOffsetConsumed('topic', 0, 43);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        ledger.onOffsetProcessed('topic', 0, 43);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        ledger.onOffsetProcessed('topic', 0, 42);
+        assert.strictEqual(ledger.getCommittableOffset('topic', 0), 44);
+    });
+
+    it('should get correct offset with ten messages processed out-of-order',
+    () => {
+        const ledger = new OffsetLedger();
+
+        const finishOrder = [46, 49, 50, 42, 51, 44, 48, 47, 43, 45];
+        const expectedCommittableOffsets =
+              [42, 42, 42, 43, 43, 43, 43, 43, 45, 52];
+
+        for (let i = 0; i < 10; ++i) {
+            ledger.onOffsetConsumed('topic', 0, 42 + i);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        }
+        for (let i = 0; i < 10; ++i) {
+            ledger.onOffsetProcessed('topic', 0, finishOrder[i]);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0),
+                               expectedCommittableOffsets[i]);
+        }
+    });
+
+    it('should get correct offset with ten messages processed intermingled ' +
+    'with ten new messages consumed', () => {
+        const ledger = new OffsetLedger();
+
+        const finishOrder =
+              [44, 50, 46, 53, 45, 42, 51, 57, 49, 56,
+               47, 43, 55, 61, 59, 54, 48, 60, 52, 58];
+
+        const expectedCommittableOffsets =
+              [42, 42, 42, 42, 42, 43, 43, 43, 43, 43,
+               43, 48, 48, 48, 48, 48, 52, 52, 58, 62];
+
+        for (let i = 0; i < 10; ++i) {
+            ledger.onOffsetConsumed('topic', 0, 42 + i);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        }
+        for (let i = 0; i < 20; ++i) {
+            ledger.onOffsetProcessed('topic', 0, finishOrder[i]);
+            if (i < 10) {
+                ledger.onOffsetConsumed('topic', 0, 52 + i);
+            }
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0),
+                               expectedCommittableOffsets[i]);
+        }
+    });
+
+    it('should get correct offset with replay of some consumed entries',
+    () => {
+        const ledger = new OffsetLedger();
+
+        const finishOrder = [46, 44, 50, 42, 51, 49, 43, 45, 47, 48];
+        const expectedCommittableOffsets =
+              [42, 42, 42, 43, 43, 43, 45, 47, 48, 49];
+
+        for (let i = 0; i < 10; ++i) {
+            ledger.onOffsetConsumed('topic', 0, 42 + i);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0), 42);
+        }
+        for (let i = 0; i < 5; ++i) {
+            ledger.onOffsetProcessed('topic', 0, finishOrder[i]);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0),
+                               expectedCommittableOffsets[i]);
+        }
+        for (let i = 5; i < 10; ++i) {
+            // introduce replayed messages
+            ledger.onOffsetConsumed('topic', 0, 42 + i);
+            ledger.onOffsetProcessed('topic', 0, finishOrder[i]);
+            assert.strictEqual(ledger.getCommittableOffset('topic', 0),
+                               expectedCommittableOffsets[i]);
+        }
+    });
+
+    it('should manage independent offsets per topic/partition', () => {
+        const ledger = new OffsetLedger();
+
+        ['topic1', 'topic2'].forEach(topic => {
+            [0, 1].forEach(partition => {
+                ledger.onOffsetConsumed(topic, partition, 42 + partition);
+                ledger.onOffsetConsumed(topic, partition, 43 + partition);
+                assert.strictEqual(
+                    ledger.getCommittableOffset(topic, partition),
+                    42 + partition);
+            });
+        });
+        ['topic2', 'topic1'].forEach(topic => {
+            [1, 0].forEach(partition => {
+                ledger.onOffsetProcessed(topic, partition, 42 + partition);
+                assert.strictEqual(
+                    ledger.getCommittableOffset(topic, partition),
+                    43 + partition);
+                ledger.onOffsetProcessed(topic, partition, 43 + partition);
+                assert.strictEqual(
+                    ledger.getCommittableOffset(topic, partition),
+                    44 + partition);
+            });
+        });
+    });
+
+    it('should be able to export the ledger in JSON format', () => {
+        const ledger = new OffsetLedger();
+
+        ['topic1', 'topic2'].forEach(topic => {
+            [0, 1].forEach(partition => {
+                ledger.onOffsetConsumed(topic, partition, 42 + partition);
+                ledger.onOffsetConsumed(topic, partition, 43 + partition);
+            });
+        });
+        ledger.onOffsetProcessed('topic1', 0, 42);
+        ledger.onOffsetProcessed('topic2', 0, 43);
+        ledger.onOffsetProcessed('topic2', 0, 42);
+
+        assert.deepStrictEqual(JSON.parse(ledger.toString()), {
+            topic1: { 0: { processing: [43], latestConsumed: 43 },
+                      1: { processing: [43, 44], latestConsumed: 44 } },
+            topic2: { 0: { processing: [], latestConsumed: 43 },
+                      1: { processing: [43, 44], latestConsumed: 44 } } });
+    });
+});
