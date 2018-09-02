1d981043c2a691054fa864867f8964d1bc8eb006
philipyoo <philip.yoo@scality.com>
2018-09-02 15:40:42 -0700

    bf: ZENKO-1105 fix ci dependency and add check
    Changes in this commit:
    - Fix cloudserver module name in package.json
    - Update package-lock
    - Add check in bash test file to make sure module was
      installed, else exit right away

--- eve/workers/unit_and_feature_tests/run_server_tests.bash
+++ eve/workers/unit_and_feature_tests/run_server_tests.bash
@@ -8,6 +8,11 @@ killandsleep () {
   sleep 10
 }
 
+if [ ! -d "node_modules/@zenko/cloudserver" ]; then
+    echo "cloudserver module was not found!"
+    exit 1
+fi
+
 # run s3 mem_backend
 cd node_modules/@zenko/cloudserver && npm run mem_backend & bash tests/utils/wait_for_local_port.bash 8000 40 && npm run ft_test
 
