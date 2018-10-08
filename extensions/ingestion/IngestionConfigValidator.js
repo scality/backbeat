a48cbca3ca1058715099c78cae96a98341919491
philipyoo <philip.yoo@scality.com>
2018-10-08 14:34:01 -0700

    add auth config

--- extensions/ingestion/IngestionConfigValidator.js
+++ extensions/ingestion/IngestionConfigValidator.js
@@ -1,6 +1,23 @@
 const joi = require('joi');
 
 const joiSchema = {
+    auth: joi.object({
+        type: joi.alternatives().try('account', 'service', 'vault')
+            .required(),
+        account: joi.string()
+            .when('type', { is: 'account', then: joi.required() })
+            .when('type', { is: 'service', then: joi.required() }),
+        vault: joi.object({
+            host: joi.string().required(),
+            port: joi.number().greater(0).required(),
+            adminPort: joi.number().greater(0)
+                .when('adminCredentialsFile', {
+                    is: joi.exist(),
+                    then: joi.required(),
+                }),
+            adminCredentialsFile: joi.string().optional(),
+        }).when('type', { is: 'vault', then: joi.required() }),
+    }).required(),
     topic: joi.string().required(),
     zookeeperPath: joi.string().required(),
     cronRule: joi.string().default('*/5 * * * * *'),
@@ -8,8 +25,7 @@ const joiSchema = {
 };
 
 function configValidator(backbeatConfig, extConfig) {
-    const validatedConfig = joi.attempt(extConfig, joiSchema);
-    return validatedConfig;
+    return joi.attempt(extConfig, joiSchema);
 }
 
 module.exports = configValidator;
