be3c21890411d3704665e3337c79ada2daf58281
philipyoo <philip.yoo@scality.com>
2018-11-05 14:07:22 -0800

    bf: ZENKO-1306 eve cloudserver dependency fix
    Zenko cloudserver package name changed to match cloudserver
    (named based on a fresh git install of the module).
    
    In bash files, do not chain server run with npm script.
    We can capture error on either step this way

--- eve/workers/unit_and_feature_tests/run_ft_tests.bash
+++ eve/workers/unit_and_feature_tests/run_ft_tests.bash
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 set -x
-set -e
+set -eu -o pipefail
+
+# port for cloudserver
+PORT=8000
 
 if [ ! -d "node_modules/@zenko/cloudserver" ]; then
     echo "cloudserver module was not found!"
     exit 1
 fi
 
+trap killandsleep EXIT
 
 killandsleep () {
-  kill -9 $(lsof -t -i:$1) || true
+  kill -9 $(lsof -t -i:$PORT) || true
   sleep 10
 }
 
-cd node_modules/@zenko/cloudserver && npm run mem_backend & bash tests/utils/wait_for_local_port.bash 8000 40 && npm run ft_test
-
-killandsleep 8000
+cd node_modules/@zenko/cloudserver && npm run mem_backend & bash tests/utils/wait_for_local_port.bash $PORT 40
+npm run ft_test
