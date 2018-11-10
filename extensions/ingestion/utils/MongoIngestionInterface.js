42b961fc4dd886bb38a761502d5f853c0a63e8d1
philipyoo <philip.yoo@scality.com>
2018-11-09 17:13:19 -0800

    ft: ZENKO-1240 ingestion bucket mongo search
    Add the ability to find ingestion buckets directly from
    mongo.

---
+++ extensions/ingestion/utils/MongoIngestionInterface.js
@@ -0,0 +1,43 @@
+const arsenal = require('arsenal');
+const Logger = require('werelogs').Logger;
+
+const { MongoClientInterface } = arsenal.storage.metadata.mongoclient;
+const errors = arsenal.errors;
+
+const USERSBUCKET = arsenal.constants.usersBucket;
+const PENSIEVE = 'PENSIEVE';
+const METASTORE = '__metastore';
+
+class MongoMDWrapper extends MongoClientInterface {
+    constructor(params) {
+        super(Object.assign({}, params, {
+            logger: new Logger('Backbeat:MongoIngestionInterface'),
+        }));
+    }
+
+    getIngestionBuckets(callback) {
+        const m = this.getCollection(METASTORE);
+        m.find({
+            '_id': {
+                $nin: [PENSIEVE, USERSBUCKET],
+            },
+            'value.ingestion': {
+                $type: 'object',
+            },
+        }, {
+            'value.name': true,
+            'value.ingestion': true,
+            'value.locationConstraint': true,
+        }).toArray((err, doc) => {
+            if (err) {
+                this.logger.error(
+                    'getIngestionBuckets: error getting ingestion buckets',
+                    { error: err.message });
+                return callback(errors.InternalError);
+            }
+            return callback(null, doc);
+        });
+    }
+}
+
+module.exports = MongoMDWrapper;
