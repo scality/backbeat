592eb2d9047720c614b25198e51b159ed0fe9e1c
bbuchanan9 <bennett.buchanan@scality.com>
2019-03-14 10:38:52 -0700

    squash: add randomized unit test

--- tests/unit/replication/ReplicationTaskScheduler.spec.js
+++ tests/unit/replication/ReplicationTaskScheduler.spec.js
@@ -35,12 +35,20 @@ function processingFunc(entry, cb) {
 
 const replicationTaskScheduler = new ReplicationTaskScheduler(processingFunc);
 
+function assertRunningTasksAreCleared() {
+    assert.deepStrictEqual(
+        Object.keys(replicationTaskScheduler._runningTasksByMasterKey), []);
+    assert.deepStrictEqual(
+        Object.keys(replicationTaskScheduler._runningTasks), []);
+}
+
 const queueEntryClasses = [
     'ObjectQueueEntry',
     'ActionQueueEntry',
 ];
 
 queueEntryClasses.forEach(mockClass => {
+    let results = {};
     let objects = [];
     let doneCount = 0;
     let queueEntryClass;
@@ -81,6 +89,32 @@ queueEntryClasses.forEach(mockClass => {
         }
     }
 
+    function scheduleDuplicateEntriesRandomized(params, cb) {
+        const { uniqueKeyCount, duplicateKeyCount } = params;
+        const hash = {};
+        for (let i = 0; i < uniqueKeyCount; ++i) {
+            const objectKey = `key_${i}`;
+            const versionId = uuid();
+            const contentMD5 = uuid();
+            for (let j = 0; j < duplicateKeyCount + 1; ++j) {
+                const setValueTo = uuid();
+                const ctx = {
+                    setValueTo,
+                    object: { objectKey, value: null, setValueTo },
+                    entry: getMockEntry({ objectKey, versionId, contentMD5 }),
+                };
+                hash[setValueTo] = ctx;
+            }
+        }
+        Object.keys(hash)
+            .sort()
+            .forEach(key => {
+                const ctx = hash[key];
+                objects.push(ctx.object);
+                replicationTaskScheduler.push(ctx, ctx.object.objectKey, cb);
+            });
+    }
+
     function testUniqueVersions(params, cb) {
         function doneFunc() {
             ++doneCount;
@@ -112,12 +146,33 @@ queueEntryClasses.forEach(mockClass => {
         scheduleEntries(params, doneFunc);
     }
 
+    function testDuplicateVersionsRandomized(params, cb) {
+        function doneFunc() {
+            ++doneCount;
+            if (doneCount !== objects.length) {
+                return undefined;
+            }
+            objects.forEach(object => {
+                if (results[object.objectKey]) {
+                    assert.strictEqual(object.value, null);
+                    assert(object.value !== object.setValueTo);
+                } else {
+                    results[object.objectKey] = true;
+                    assert.strictEqual(object.value, object.setValueTo);
+                }
+            });
+            return cb();
+        }
+        scheduleDuplicateEntriesRandomized(params, doneFunc);
+    }
+
     describe(`task scheduler with ${mockClass}`, () => {
         before(() => {
             queueEntryClass = mockClass;
         });
 
         beforeEach(() => {
+            results = {};
             objects = [];
             doneCount = 0;
         });
@@ -150,14 +205,39 @@ queueEntryClasses.forEach(mockClass => {
                 }, done);
             });
 
-        it('should ensure serialization if duplicate keys and versions',
+        it('should ensure serialization if duplicate entries: ordered',
             done => {
                 testDuplicateVersions({
-                    uniqueKeyCount: 10,
-                    duplicateKeyCount: 9,
+                    uniqueKeyCount: 2,
+                    duplicateKeyCount: 1,
                     hasUniqueVersions: false,
                     hasUniqueContent: false,
                 }, done);
+            });
+
+        it('should ensure serialization if duplicate entries: randomized',
+            done => {
+                testDuplicateVersionsRandomized({
+                    uniqueKeyCount: 10,
+                    duplicateKeyCount: 9,
+                }, done);
+            });
+
+        it('should clear the running task queues and filters when finished',
+            done => {
+                testDuplicateVersionsRandomized({
+                    uniqueKeyCount: 10,
+                    duplicateKeyCount: 9,
+                }, err => {
+                    if (err) {
+                        return done(err);
+                    }
+                    // Must wait some time for all queues to finish running.
+                    return setTimeout(() => {
+                        assertRunningTasksAreCleared();
+                        done();
+                    }, 1000);
+                });
             });
 
         it('should ensure serialization if duplicate keys and a combination ' +
