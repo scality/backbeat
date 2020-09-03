996a563af0dc69cf33dab644e9c8fed4a6e01e54
naren-scality <narendran.rajendran@scality.com>
2020-09-03 15:57:07 -0700

    improvement: S3C-3316 modify notification message
    Bucket notification message structure is modified to match aws
    specifications.

---
+++ extensions/notification/utils/message.js
@@ -0,0 +1,79 @@
+const { versioning } = require('arsenal');
+const versionIdUtils = versioning.VersionID;
+
+const constants = require('../constants');
+
+/**
+ * Add attributes to an internal notification message,
+ * with its log entry counterpart.
+ * @param  {Object} logEntry - log entry that generated the message
+ * @param  {Object} message - initial message generated
+ * @return {Object} updated message object
+ */
+function addLogAttributes(logEntry, message) {
+    const { eventMessageProperty } = constants;
+    const msg = {};
+    for (const [key, value] of Object.entries(eventMessageProperty)) {
+        let val = logEntry[value];
+        if (typeof val === 'number') {
+            val = val.toString();
+        }
+        msg[key] = val || null;
+    }
+    return Object.assign(msg, message);
+}
+
+/**
+ * Transforms an event object to specification.
+ * @param  {Object} event - event object
+ * @return {Object} updated event object
+ */
+function transformToSpec(event) {
+    const encodedVersionId
+        = event.versionId ? versionIdUtils.encode(event.versionId) : null;
+    const message = {
+        Records: [
+            {
+                eventVersion: constants.eventVersion,
+                eventSource: constants.eventSource,
+                awsRegion: event.region,
+                eventTime: event.dateTime,
+                eventName: event.eventType,
+                userIdentity: {
+                    principalId: null,
+                },
+                requestParameters: {
+                    sourceIPAddress: null,
+                },
+                responseElements: {
+                    'x-amz-request-id': null,
+                    'x-amz-id-2': null,
+                },
+                s3: {
+                    s3SchemaVersion: constants.eventS3SchemaVersion,
+                    configurationId: event.configurationId,
+                    bucket: {
+                        name: event.bucket,
+                        ownerIdentity: {
+                            principalId: null,
+                        },
+                        arn: null,
+                    },
+                    object: {
+                        key: event.key,
+                        size: event.size,
+                        eTag: null,
+                        versionId: encodedVersionId,
+                        sequencer: null,
+                    },
+                },
+            },
+        ],
+    };
+    return message;
+}
+
+module.exports = {
+    addLogAttributes,
+    transformToSpec,
+};
---
+++ tests/unit/notification/utils/message.js
@@ -0,0 +1,109 @@
+/* eslint-disable quote-props */
+const assert = require('assert');
+
+const messageUtil
+    = require('../../../../extensions/notification/utils/message');
+const notifConstants
+    = require('../../../../extensions/notification/constants');
+
+const { eventMessageProperty } = notifConstants;
+const logEntryValue = {
+    acl: {
+        Canned: 'authenticated-read',
+        FULL_CONTROL: [],
+        WRITE: [],
+        WRITE_ACP: [],
+        READ: [],
+        READ_ACP: [],
+    },
+    key: '',
+    location: [{
+        key: '70FBD0849D7CEA7CC2759C384876725991169320',
+        size: 11,
+        start: 0,
+        dataStoreName: 'us-east-1',
+        dataStoreType: 'scality',
+        dataStoreETag: '1:3749f52bb326ae96782b42dc0a97b4c1',
+    }],
+    isDeleteMarker: false,
+    tags: {},
+    replicationInfo: {
+        status: '',
+        backends: [],
+        content: [],
+        destination: '',
+        storageClass: '',
+        role: '',
+        storageType: '',
+        dataStoreVersionId: '',
+    },
+    dataStoreName: 'us-east-1',
+    originOp: 's3:ObjectCreated:Put',
+    'last-modified': '2020-08-30T05:27:08.075Z',
+    'md-model-version': 3,
+    versionId: '98401234771912999999RG001  7.14.2',
+    'owner-display-name': 'owner',
+    'owner-id':
+        '9abe0ec063308bc0c7cd5f99e1260f942afb7e8682a86edc807795fa652ee28a',
+    'content-length': 11,
+    'content-type': 'text/plain',
+    'content-md5': '3749f52bb326ae96782b42dc0a97b4c1',
+    'x-amz-version-id': 'null',
+    'x-amz-server-version-id': '',
+    'x-amz-storage-class': 'STANDARD',
+    'x-amz-server-side-encryption': '',
+    'x-amz-server-side-encryption-aws-kms-key-id': '',
+    'x-amz-server-side-encryption-customer-algorithm': '',
+    'x-amz-website-redirect-location': '',
+};
+
+const testEntry = {
+    bucket: 'bucket1',
+    key: 'test.txt',
+    eventType: 's3:ObjectCreated:Put',
+    versionId: '98401234771912999999RG001  7.14.2',
+    configurationId: 'config1',
+};
+
+describe('Notification message util', () => {
+    describe('AddLogAttributes', () => {
+        it('should add attributes to a notification entry', () => {
+            const message
+                = messageUtil.addLogAttributes(logEntryValue, testEntry);
+            assert(message);
+            for (const [key, value] of Object.entries(eventMessageProperty)) {
+                assert.equal(message[key], logEntryValue[value]);
+            }
+        });
+        // event type for non-versioned object delete will not be available
+        // in log, so it is set before adding attributes
+        it('should not modify event type if log entry has different type',
+            () => {
+                const entry = Object.assign(testEntry, {
+                    eventType: 's3:ObjectRemoved:Delete',
+                });
+                const message
+                    = messageUtil.addLogAttributes(logEntryValue, entry);
+                assert.strictEqual(message.eventType, entry.eventType);
+            }
+        );
+    });
+    describe('TransformToSpec', () => {
+        it('should add transform the event source entry to spec', () => {
+            const eventEntry
+                = messageUtil.addLogAttributes(logEntryValue, testEntry);
+            const message
+                = messageUtil.transformToSpec(eventEntry);
+            assert(message);
+            assert(message.Records[0]);
+            const record = message.Records[0];
+            assert(record.requestParameters);
+            assert(record.responseElements);
+            assert(record.s3);
+            const s3 = record.s3;
+            assert(s3.bucket);
+            assert(s3.object);
+            assert.notStrictEqual(s3.object.versionId, eventEntry.versionId);
+        });
+    });
+});
