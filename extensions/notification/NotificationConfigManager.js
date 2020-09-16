7068f86f576812790f996b801157531793c3926e
naren-scality <narendran.rajendran@scality.com>
2020-09-15 20:48:36 -0700

    improvement: S3C-3331 sync zookeeper calls
    A new configuration manager is introduced to handle zookeeper calls in
    the background and manage bucket notification configuration
    synchronously.

---
+++ extensions/notification/NotificationConfigManager.js
@@ -0,0 +1,402 @@
+const async = require('async');
+const { EventEmitter } = require('events');
+const zookeeper = require('node-zookeeper-client');
+const joi = require('@hapi/joi');
+
+const safeJsonParse = require('./utils/safeJsonParse');
+const constants = require('./constants');
+
+const paramsJoi = joi.object({
+    zkClient: joi.object().required(),
+    parentNode: joi.string().optional(),
+    logger: joi.object().required(),
+}).required();
+
+/**
+ * @class NotificationConfigManager
+ *
+ * @classdesc Manages bucket notification configurations in zookeeper, maintains
+ * configurations across multiple managers.
+ */
+class NotificationConfigManager {
+    /**
+     * @constructor
+     * @param {Object} params - constructor params
+     * @param {Object} params.zkClient - zookeeper client
+     * @param {String} params.parentNode - parent node to be used
+     * @param {Logger} params.logger - logger object
+     */
+    constructor(params) {
+        joi.attempt(params, paramsJoi);
+        this._zkClient = params.zkClient;
+        this._parentNode = params.parentNode || '';
+        this.log = params.logger;
+        this._configs = new Map();
+        this._concurrency = constants.configManager.concurrency;
+        this._emitter = new EventEmitter();
+        this._setupEmitterListeners();
+    }
+
+    _emitterErrorListener(error, listener) {
+        this.log.error('BucketNotifConfigManager.emitter.error', {
+            listener,
+            error,
+        });
+        return undefined;
+    }
+
+    _setConfigListener(bucket, config) {
+        this.log.debug('BucketNotifConfigManager.emitter.setConfig', {
+            event: 'setConfig',
+            bucket,
+            config,
+        });
+        this._setBucketNotifConfig(bucket, JSON.stringify(config), err => {
+            if (err) {
+                this._emitter.emit('error', err, 'setConfigListener');
+            }
+            return undefined;
+        });
+    }
+
+    _getConfigListener(updatedBucket = '') {
+        this.log.debug('BucketNotifConfigManager.emitter.getConfig', {
+            event: 'getConfig',
+        });
+        this._listBucketsWithConfig((err, buckets) => {
+            if (err) {
+                this._emitter.emit('error', err, 'getConfigListener');
+                return undefined;
+            }
+            this.log.debug('bucket config to be updated in map', {
+                bucket: updatedBucket,
+            });
+            const newBuckets
+                = this._getNewBucketNodes(buckets);
+            this.log.debug('new bucket configs to be added to map', {
+                buckets: newBuckets,
+            });
+            const bucketsToMap
+                = updatedBucket ?
+                    [updatedBucket, ...newBuckets] : newBuckets;
+            this.log.debug('bucket configs to be added/updated to map', {
+                buckets: bucketsToMap,
+            });
+            if (bucketsToMap.length > 0) {
+                this._updateLocalStore(bucketsToMap);
+            }
+            return undefined;
+        });
+    }
+
+    _removeConfigListener(bucket) {
+        this.log.debug('BucketNotifConfigManager.emitter.removeConfig', {
+            event: 'removeConfig',
+            bucket,
+        });
+        this._removeBucketNotifConfigNode(bucket, err => {
+            if (err) {
+                this._emitter.emit('error', err, 'removeConfigListener');
+            }
+            return undefined;
+        });
+    }
+
+    _setupEmitterListeners() {
+        this._emitter.setMaxListeners(constants.configManager.maxListeners);
+        this._emitter
+            .on('error', error => this._emitterErrorListener(error))
+            .on('setConfig',
+                (bucket, config) => this._setConfigListener(bucket, config))
+            .on('getConfig', bucket => this._getConfigListener(bucket))
+            .on('removeConfig', bucket => this._removeConfigListener(bucket));
+    }
+
+    _callbackHandler(cb, err, result) {
+        if (cb && typeof cb === 'function') {
+            return cb(err, result);
+        }
+        return undefined;
+    }
+
+    _getBucketNodeZkPath(bucket) {
+        if (this._parentNode) {
+            return `/${this._parentNode}/${bucket}`;
+        }
+        return `/${bucket}`;
+    }
+
+    _getConfigDataFromBuffer(data) {
+        const { error, result } = safeJsonParse(data);
+        if (error) {
+            this.log.error('invalid config', { error, config: data });
+            return undefined;
+        }
+        return result;
+    }
+
+    _getBucketNotifConfig(bucket, cb) {
+        const method
+            = 'BucketNotificationConfigManager._getBucketNotifConfig';
+        const zkPath = this._getBucketNodeZkPath(bucket);
+        return this._zkClient.getData(zkPath, event => {
+            this.log.debug('zookeeper getData watcher triggered', {
+                zkPath,
+                method,
+                event,
+                bucket,
+            });
+            if (event.type === zookeeper.Event.NODE_DATA_CHANGED) {
+                this._emitter.emit('getConfig', bucket);
+            }
+            if (event.type === zookeeper.Event.NODE_DELETED) {
+                this.removeConfig(bucket, false);
+            }
+        }, (error, data) => {
+            if (error && error.name !== 'NO_NODE') {
+                const errMsg
+                    = 'error fetching bucket notification configuration';
+                this.log.error(errMsg, {
+                    method,
+                    error,
+                });
+                return this._callbackHandler(cb, error);
+            }
+            if (data) {
+                return this._callbackHandler(cb, null, data);
+            }
+            // no configuration
+            return this._callbackHandler(cb);
+        });
+    }
+
+    _checkNodeExists(zkPath, cb) {
+        const method
+            = 'BucketNotificationConfigManager._checkNodeExists';
+        return this._zkClient.exists(zkPath, (err, stat) => {
+            if (err) {
+                this.log.error('error checking node existence',
+                    { method, zkPath });
+                return this._callbackHandler(cb, err);
+            }
+            if (stat) {
+                this.log.debug('node exists', { method, zkPath });
+                return this._callbackHandler(cb, null, true);
+            }
+            this.log.debug('node does not exist', { method, zkPath });
+            return this._callbackHandler(cb, null, false);
+        });
+    }
+
+    _setBucketNotifConfig(bucket, data, cb) {
+        const method
+            = 'BucketNotificationConfigManager._setBucketNotifConfig';
+        const zkPath = this._getBucketNodeZkPath(bucket);
+        return async.waterfall([
+            next => this._checkNodeExists(zkPath, next),
+            (exists, next) => {
+                if (!exists) {
+                    return this._createBucketNotifConfigNode(bucket,
+                        err => next(err));
+                }
+                return next();
+            },
+            next => this._zkClient.setData(zkPath, Buffer.from(data), -1, next),
+        ], err => {
+            if (err) {
+                this.log.error('error saving config', { method, zkPath, data });
+            }
+            return this._callbackHandler(cb, err);
+        });
+    }
+
+    _createBucketNotifConfigNode(bucket, cb) {
+        const method
+            = 'BucketNotificationConfigManager._createBucketNotifConfigNode';
+        const zkPath = this._getBucketNodeZkPath(bucket);
+        return this._zkClient.mkdirp(zkPath, err => {
+            if (err) {
+                this.log.error('Could not pre-create path in zookeeper', {
+                    method,
+                    zkPath,
+                    error: err,
+                });
+                return this._callbackHandler(cb, err);
+            }
+            return this._callbackHandler(cb);
+        });
+    }
+
+    _removeBucketNotifConfigNode(bucket, cb) {
+        const method
+            = 'BucketNotificationConfigManager._removeBucketNotifConfigNode';
+        const zkPath = this._getBucketNodeZkPath(bucket);
+        return this._zkClient.remove(zkPath, error => {
+            if (error && error.name !== 'NO_NODE') {
+                this.log.error('Could not remove zookeeper node', {
+                    method,
+                    zkPath,
+                    error,
+                });
+                return this._callbackHandler(cb, error);
+            }
+            if (!error) {
+                const msg
+                    = 'removed notification configuration zookeeper node';
+                this.log.debug(msg, {
+                    method,
+                    bucket,
+                });
+            }
+            return this._callbackHandler(cb);
+        });
+    }
+
+    _getNewBucketNodes(bucketsNodeList) {
+        if (Array.isArray(bucketsNodeList)) {
+            const bucketsFromMap = [...this._configs.keys()];
+            return bucketsNodeList.filter(b => !bucketsFromMap.includes(b));
+        }
+        return [];
+    }
+
+    _listBucketsWithConfig(cb) {
+        const method
+            = 'BucketNotificationConfigManager._listBucketsWithConfig';
+        const zkPath = `/${this._parentNode}`;
+        this._zkClient.getChildren(zkPath, event => {
+            this.log.debug('zookeeper getChildren watcher triggered', {
+                zkPath,
+                method,
+                event,
+            });
+            if (event.type === zookeeper.Event.NODE_CHILDREN_CHANGED) {
+                this._emitter.emit('getConfig');
+            }
+        }, (error, buckets) => {
+            if (error) {
+                const errMsg
+                    = 'error listing buckets with configuration';
+                this.log.error(errMsg, {
+                    zkPath,
+                    method,
+                    error,
+                });
+                this._callbackHandler(cb, error);
+            }
+            this._callbackHandler(cb, null, buckets);
+        });
+    }
+
+    _updateLocalStore(buckets, cb) {
+        async.eachSeries(buckets, (bucket, next) => {
+            this._getBucketNotifConfig(bucket, (err, data) => {
+                if (err) {
+                    next(err);
+                }
+                const configObject = this._getConfigDataFromBuffer(data);
+                if (configObject) {
+                    this._configs.set(bucket, configObject);
+                }
+                next();
+            });
+        }, err => this._callbackHandler(cb, err));
+    }
+
+    /**
+     * Get bucket notification configuration
+     *
+     * @param {String} bucket - bucket
+     * @return {Object|undefined} - configuration if available or undefined
+     */
+    getConfig(bucket) {
+        return this._configs.get(bucket);
+    }
+
+    /**
+     * Add/update bucket notification configuration
+     *
+     * @param {String} bucket - bucket
+     * @param {Object} config - bucket notification configuration
+     * @return {boolean|undefined} - true if removed or undefined
+     */
+    setConfig(bucket, config) {
+        try {
+            this.log.debug('set config', {
+                method: 'BucketNotificationConfigManager.setConfig',
+                bucket,
+                config,
+            });
+            this._configs.set(bucket, config);
+            this._emitter.emit('setConfig', bucket, config);
+            return true;
+        } catch (err) {
+            const errMsg
+                = 'error setting bucket notification configuration';
+            this.log.error(errMsg, {
+                method: 'BucketNotificationConfigManager.setConfig',
+                error: err,
+                bucket,
+                config,
+            });
+            return undefined;
+        }
+    }
+
+    /**
+     * Remove bucket notification configuration
+     *
+     * @param {String} bucket - bucket
+     * @param {boolean} removeNode - remove the zookeeper node (optional)
+     * @return {boolean|undefined} - true if removed or undefined
+     */
+    removeConfig(bucket, removeNode = true) {
+        try {
+            this.log.debug('remove config', {
+                method: 'BucketNotificationConfigManager.removeConfig',
+                bucket,
+                removeNode,
+            });
+            this._configs.delete(bucket);
+            if (removeNode) {
+                this._emitter.emit('removeConfig', bucket);
+            }
+            return true;
+        } catch (err) {
+            const errMsg
+                = 'error removing bucket notification configuration';
+            this.log.error(errMsg, {
+                method: 'BucketNotificationConfigManager.setConfig',
+                error: err,
+                bucket,
+            });
+            return undefined;
+        }
+    }
+
+    /**
+     * Get the list of buckets with notification configurations
+     *
+     * @return {Array} - an array of bucket names
+     */
+    getBucketsWithConfigs() {
+        return [...this._configs.keys()];
+    }
+
+    /**
+     * Init bucket notification configuration manager
+     *
+     * @param {function} [cb] - callback
+     * @return {undefined}
+     */
+    init(cb) {
+        this._listBucketsWithConfig((err, buckets) => {
+            if (err) {
+                cb(err);
+            }
+            this._updateLocalStore(buckets, cb);
+        });
+    }
+}
+
+module.exports = NotificationConfigManager;
---
+++ tests/functional/notification/NotificationConfigManager.js
@@ -0,0 +1,161 @@
+const assert = require('assert');
+const async = require('async');
+const werelogs = require('werelogs');
+const ZookeeperMock = require('zookeeper-mock');
+
+const NotificationConfigManager
+    = require('../../../extensions/notification/NotificationConfigManager');
+
+const logger = new werelogs.Logger('NotificationConfigManager:test');
+const zkConfigParentNode = 'config';
+const concurrency = 10;
+const bucketPrefix = 'bucket';
+const timeoutMs = 100;
+
+function getTestConfigValue(bucket) {
+    const data = {
+        bucket,
+        notificationConfiguration: {
+            queueConfig: [
+                {
+                    events: ['s3:ObjectCreated:Put'],
+                    queueArn: 'arn:scality:bucketnotif:::destination1',
+                    filterRules: [],
+                    id: `${zkConfigParentNode}-${bucket}`,
+                },
+            ],
+        },
+    };
+    return data;
+}
+
+function populateTestConfigs(zkClient, numberOfConfigs, cb) {
+    async.timesLimit(numberOfConfigs, concurrency, (n, done) => {
+        const bucket = `${bucketPrefix}${n + 1}`;
+        const val = getTestConfigValue(bucket);
+        const strVal = JSON.stringify(val);
+        const node = `/${zkConfigParentNode}/${bucket}`;
+        async.series([
+            nxt => zkClient.mkdirp(node, nxt),
+            // nxt => zkClient.setData(node, strVal, nxt),
+            nxt => zkClient.setData(node, Buffer.from(strVal), nxt),
+        ], done);
+    }, cb);
+}
+
+function listBuckets(zkClient, cb) {
+    const node = `/${zkConfigParentNode}`;
+    zkClient.getChildren(node, cb);
+}
+
+function deleteTestConfigs(zkClient, cb) {
+    const node = `/${zkConfigParentNode}`;
+    listBuckets(zkClient, (e, children) => {
+        if (e) {
+            assert.ifError(e);
+            cb(e);
+        } else {
+            async.eachLimit(children, concurrency, (child, next) => {
+                const childNode = `${node}/${child}`;
+                zkClient.remove(childNode, next);
+            }, cb);
+        }
+    });
+}
+
+describe('NotificationConfigManager multiple managers functional tests', () => {
+    const zkClient = new ZookeeperMock();
+    const params = {
+        zkClient,
+        parentNode: zkConfigParentNode,
+        logger,
+    };
+    let configManager1 = null;
+    let configManager2 = null;
+
+    function checkCount() {
+        const buckets1 = configManager1.getBucketsWithConfigs();
+        const buckets2 = configManager2.getBucketsWithConfigs();
+        assert.strictEqual(buckets1.length, buckets2.length);
+    }
+
+    function checkBucketConfig(bucket) {
+        const existingConfig1 = configManager1.getConfig(bucket);
+        const existingConfig2 = configManager2.getConfig(bucket);
+        assert.deepStrictEqual(existingConfig1, existingConfig2);
+    }
+
+    beforeEach(done => {
+        configManager1 = new NotificationConfigManager(params);
+        configManager2 = new NotificationConfigManager(params);
+        populateTestConfigs(zkClient, 5, () => {
+            async.series([
+                next => configManager1.init(next),
+                next => configManager2.init(next),
+            ], done);
+        });
+    });
+
+    afterEach(() => {
+        zkClient._resetState();
+    });
+
+    it('managers should have the same config values after init', done => {
+        listBuckets(zkClient, (err, buckets) => {
+            assert.ifError(err);
+            buckets.forEach(bucket => {
+                checkBucketConfig(bucket);
+            });
+            checkCount();
+            done();
+        });
+    });
+
+    it('managers should have the config if a new one is added', done => {
+        const bucket = 'bucket100';
+        const config = getTestConfigValue(bucket);
+        const result = configManager1.setConfig(bucket, config);
+        assert(result);
+        setTimeout(() => {
+            checkBucketConfig(bucket);
+            checkCount();
+            done();
+        }, timeoutMs);
+    });
+
+    it('managers should have the same updated config value', done => {
+        const bucket = 'bucket1';
+        checkBucketConfig(bucket);
+        const config = getTestConfigValue(`${bucket}-updated`);
+        const result = configManager1.setConfig(bucket, config);
+        assert(result);
+        setTimeout(() => {
+            checkBucketConfig(bucket);
+            checkCount();
+            done();
+        }, timeoutMs);
+    });
+
+    it('managers should have the same cofig count after a config is removed',
+        done => {
+            const bucket = 'bucket1';
+            const result = configManager1.removeConfig(bucket);
+            assert(result);
+            setTimeout(() => {
+                checkBucketConfig(bucket);
+                checkCount();
+                done();
+            }, timeoutMs);
+        });
+
+    it('managers should have the same config count if all configs are removed',
+        done => {
+            deleteTestConfigs(zkClient, err => {
+                assert.ifError(err);
+                setTimeout(() => {
+                    checkCount();
+                    done();
+                }, timeoutMs);
+            });
+        });
+});
