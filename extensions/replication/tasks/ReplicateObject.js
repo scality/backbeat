3cf49887927f11d301392caf49f3ea7f2c2dd5dc
Jonathan Gramain <jonathan.gramain@scality.com>
2020-12-07 11:00:43 -0800

    [experimental] skip replication already COMPLETED

--- extensions/replication/tasks/ReplicateObject.js
+++ extensions/replication/tasks/ReplicateObject.js
@@ -14,6 +14,9 @@ const AccountCredentials =
 const RoleCredentials =
           require('../../../lib/credentials/RoleCredentials');
 const { metricsExtension, metricsTypeProcessed } = require('../constants');
+const ObjectQueueEntry = require('../utils/ObjectQueueEntry');
+
+const errorAlreadyCompleted = {};
 
 function _extractAccountIdFromRole(role) {
     return role.split(':')[4];
@@ -288,27 +291,65 @@ class ReplicateObject extends BackbeatTask {
             });
     }
 
-    _getAndPutData(sourceEntry, destEntry, log, cb) {
-        log.debug('replicating data', { entry: sourceEntry.getLogInfo() });
-        if (sourceEntry.getLocation().some(part => {
-            const partObj = new ObjectMDLocation(part);
-            return partObj.getDataStoreETag() === undefined;
-        })) {
-            log.error('cannot replicate object without dataStoreETag ' +
-                      'property',
-                      { method: 'ReplicateObject._getAndPutData',
-                        entry: sourceEntry.getLogInfo() });
-            return cb(errors.InvalidObjectState);
-        }
-        const locations = sourceEntry.getReducedLocations();
-        const mpuConcLimit = this.repConfig.queueProcessor.mpuPartsConcurrency;
-        return mapLimitWaitPendingIfError(locations, mpuConcLimit, (part, done) => {
-            this._getAndPutPart(sourceEntry, destEntry, part, log, done);
-        }, (err, destLocations) => {
+    _refreshSourceEntry(sourceEntry, log, cb) {
+        const params = {
+            Bucket: sourceEntry.getBucket(),
+            Key: sourceEntry.getObjectKey(),
+            VersionId: sourceEntry.getEncodedVersionId(),
+        };
+        return this.backbeatSource.getMetadata(params, (err, blob) => {
             if (err) {
-                return this._deleteOrphans(sourceEntry, destLocations, log, () => cb(err));
+                log.error('error getting metadata blob from S3', {
+                    method: 'ReplicateObject._refreshSourceEntry',
+                    error: err,
+                });
+                return cb(err);
             }
-            return cb(null, destLocations);
+            const parsedEntry = ObjectQueueEntry.createFromBlob(blob.Body);
+            if (parsedEntry.error) {
+                log.error('error parsing metadata blob', {
+                    error: parsedEntry.error,
+                    method: 'ReplicateObject._refreshSourceEntry',
+                });
+                return cb(errors.InternalError.
+                    customizeDescription('error parsing metadata blob'));
+            }
+            const refreshedEntry = new ObjectQueueEntry(sourceEntry.getBucket(),
+                sourceEntry.getObjectVersionedKey(), parsedEntry.result);
+            return cb(null, refreshedEntry);
+        });
+    }
+
+    _getAndPutData(sourceEntry, destEntry, log, cb) {
+        this._refreshSourceEntry(sourceEntry, log, (err, refreshedEntry) => {
+            if (err) {
+                return cb(err);
+            }
+            const status = refreshedEntry.getReplicationSiteStatus(this.site);
+            if (status === 'COMPLETED') {
+                return cb(errorAlreadyCompleted);
+            }
+            log.debug('replicating data', { entry: sourceEntry.getLogInfo() });
+            if (sourceEntry.getLocation().some(part => {
+                const partObj = new ObjectMDLocation(part);
+                return partObj.getDataStoreETag() === undefined;
+            })) {
+                log.error('cannot replicate object without dataStoreETag ' +
+                          'property',
+                          { method: 'ReplicateObject._getAndPutData',
+                            entry: sourceEntry.getLogInfo() });
+                return cb(errors.InvalidObjectState);
+            }
+            const locations = sourceEntry.getReducedLocations();
+            const mpuConcLimit = this.repConfig.queueProcessor.mpuPartsConcurrency;
+            return mapLimitWaitPendingIfError(locations, mpuConcLimit, (part, done) => {
+                this._getAndPutPart(sourceEntry, destEntry, part, log, done);
+            }, (err, destLocations) => {
+                if (err) {
+                    return this._deleteOrphans(sourceEntry, destLocations, log, () => cb(err));
+                }
+                return cb(null, destLocations);
+            });
         });
     }
 
@@ -627,6 +668,12 @@ class ReplicateObject extends BackbeatTask {
                     entry: sourceEntry.getLogInfo(),
                     origin: err.origin,
                     error: err.description });
+            return done();
+        }
+        if (err === errorAlreadyCompleted) {
+            log.warn('replication skipped: ' +
+                     'source object version already COMPLETED',
+                     { entry: sourceEntry.getLogInfo() });
             return done();
         }
         if (err.ObjNotFound || err.code === 'ObjNotFound') {
