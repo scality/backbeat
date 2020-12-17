eadb62d97b28397f7d286e779846dbb1f1f5f8a4
Rahul Padigela <rahul.padigela@scality.com>
2020-12-17 10:30:07 -0800

    improvement: S3C-3727 remove deprecated buffer usage

--- tests/unit/lib/util/buffer.spec.js
+++ tests/unit/lib/util/buffer.spec.js
@@ -4,14 +4,14 @@ const { readUInt64BE } = require('../../../../lib/util/buffer');
 
 describe('readUInt64BE', () => {
     it('should parse a small long number in big endian', () => {
-        const tsBuf = new Buffer([0, 0, 0, 0, 0, 0, 0, 3]);
+        const tsBuf = Buffer.from([0, 0, 0, 0, 0, 0, 0, 3]);
         const value = readUInt64BE(tsBuf);
         assert.strictEqual(value, 3);
     });
 
     it('should parse a long number greater than 2**32 in big endian', () => {
         // taken from an actual timestamp value from a zookeeper node
-        const tsBuf = new Buffer([0, 0, 1, 107, 118, 199, 180, 164]);
+        const tsBuf = Buffer.from([0, 0, 1, 107, 118, 199, 180, 164]);
         const value = readUInt64BE(tsBuf);
         assert.strictEqual(value, 1561065927844);
     });
