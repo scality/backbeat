1a099a86b7ac478fe7978cd019d5b31d51be9672
Ronnie Smith <halfpint1170@gmail.com>
2021-05-10 16:59:02 -0700

    feature: S3C-4331 Add tests with sinon

--- tests/unit/QueuePopulator.spec.js
+++ tests/unit/QueuePopulator.spec.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 
 const { Logger } = require('werelogs');
+const sinon = require('sinon');
 
+const zookeeper = require('node-zookeeper-client');
 const QueuePopulator = require('../../lib/queuePopulator/QueuePopulator');
 const LogReader = require('../../lib/queuePopulator/LogReader');
 
@@ -37,16 +39,117 @@ class MockLogReader extends LogReader {
 }
 
 describe('QueuePopulator', () => {
-    it('should stop processing old raft sessions after provisioning update', done => {
-        const qp = new QueuePopulator({}, {}, {
+    let qp;
+    beforeEach(() => {
+        qp = new QueuePopulator({}, {}, {
             logSource: 'bucketd',
         }, null, null, null, {});
+    });
+
+    it('should stop processing old raft sessions after provisioning update', done => {
         const logReader1 = new MockLogReader(qp, 'raft_1');
         qp.logReadersUpdate = [logReader1];
         qp.processAllLogEntries({}, err => {
             assert.ifError(err);
             assert.strictEqual(logReader1.processLogEntriesCallCount, 2);
             done();
+        });
+    });
+
+    describe.only('handle liveness', () => {
+        let mockRes;
+        let mockLog;
+        beforeEach(() => {
+            mockRes = sinon.spy();
+            mockLog = sinon.spy();
+            // create children spies?
+            mockLog.debug = sinon.spy();
+            mockRes.writeHead = sinon.spy();
+            mockRes.end = sinon.spy();
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('success', () => {
+            qp.zkClient = {
+                getState: () => zookeeper.State.SYNC_CONNECTED,
+            };
+            const response = qp.handleLiveness(mockRes, mockLog);
+            assert.strictEqual(response, undefined);
+            sinon.assert.calledOnceWithExactly(
+                mockLog.debug,
+                sinon.match.any, // we don't care about the debug label
+                { zookeeper: zookeeper.State.SYNC_CONNECTED.code }
+            );
+            sinon.assert.calledOnceWithExactly(mockRes.writeHead, 200);
+            sinon.assert.calledOnce(mockRes.end);
+        });
+
+        it('success and logs producers', () => {
+            const prodStatus = {
+                topicA: true,
+                topicB: true,
+            };
+            const logInfo = {
+                name: 'random name',
+            };
+            const mockLogReader = sinon.spy();
+            mockLogReader.getProducerStatus = sinon.fake(() => prodStatus);
+            mockLogReader.getLogInfo = sinon.fake(() => logInfo);
+            qp.logReaders = [
+                mockLogReader,
+            ];
+            qp.zkClient = {
+                getState: () => zookeeper.State.SYNC_CONNECTED,
+            };
+            const response = qp.handleLiveness(mockRes, mockLog);
+            assert.strictEqual(response, undefined);
+            sinon.assert.calledOnceWithExactly(
+                mockLog.debug,
+                sinon.match.any, // we don't care about the debug label
+                {
+                    'zookeeper': zookeeper.State.SYNC_CONNECTED.code,
+                    'producer-topicA': true,
+                    'producer-topicB': true,
+                }
+            );
+            sinon.assert.calledOnceWithExactly(mockRes.writeHead, 200);
+            sinon.assert.calledOnce(mockRes.end);
+        });
+
+        it('returns verbose details on errors', () => {
+            const mockLogReader = sinon.spy();
+            const prodStatus = {
+                topicA: true,
+                topicB: false,
+            };
+            const logInfo = {
+                name: 'random name 2',
+            };
+            mockLogReader.getProducerStatus = sinon.fake(() => prodStatus);
+            mockLogReader.getLogInfo = sinon.fake(() => logInfo);
+            qp.logReaders = [
+                mockLogReader,
+            ];
+            qp.zkClient = {
+                getState: () => zookeeper.State.SYNC_CONNECTED,
+            };
+            const response = qp.handleLiveness(mockRes, mockLog);
+            assert.notStrictEqual(response.indexOf('topicB'), -1);
+            assert.notStrictEqual(response.indexOf('is not ready'), -1);
+            sinon.assert.calledOnceWithExactly(
+                mockLog.debug,
+                sinon.match.any, // we don't care about the debug label
+                {
+                    'zookeeper': zookeeper.State.SYNC_CONNECTED.code,
+                    'producer-topicA': true,
+                    'producer-topicB': false,
+                }
+            );
+            sinon.assert.notCalled(mockRes.writeHead);
+            sinon.assert.notCalled(mockRes.end);
         });
     });
 });
