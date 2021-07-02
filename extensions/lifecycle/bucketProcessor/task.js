a24b72f476a41578bda3023025f3bb6186451d1a
Alexander Chan <alexander.chan@scality.com>
2021-07-02 01:21:59 -0700

    S3C-4594: update lifecycle bucket processor

--- extensions/lifecycle/bucketProcessor/task.js
+++ extensions/lifecycle/bucketProcessor/task.js
@@ -1,14 +1,20 @@
 'use strict'; // eslint-disable-line
+
+const async = require('async');
 const werelogs = require('werelogs');
-const { HealthProbeServer } = require('arsenal').network.probe;
-const { initManagement } = require('../../../lib/management/index');
+const { errors } = require('arsenal');
+const {
+    DEFAULT_LIVE_ROUTE,
+    DEFAULT_READY_ROUTE,
+} = require('arsenal').network.probe.ProbeServer;
+
 const LifecycleBucketProcessor = require('./LifecycleBucketProcessor');
-const { applyBucketLifecycleWorkflows } = require('../management');
+const { sendSuccess, sendError, startProbeServer } = require('../../../lib/util/probe');
 const config = require('../../../conf/Config');
 
-const {
-    zookeeper, kafka, extensions, s3, transport, log, healthcheckServer,
-} = config;
+const { zookeeper, kafka, extensions, s3, log } = config;
+const lcConfig = extensions.lifecycle;
+const repConfig = extensions.replication;
 
 werelogs.configure({
     level: log.logLevel,
@@ -17,55 +23,46 @@ werelogs.configure({
 
 const logger = new werelogs.Logger('Backbeat:Lifecycle:Producer');
 
-const bucketProcessor = new LifecycleBucketProcessor(zookeeper, kafka, extensions, s3, transport);
+const bucketProcessor = new LifecycleBucketProcessor(
+    zookeeper, kafka, lcConfig, repConfig, s3, lcConfig.transport
+);
 
-const healthServer = new HealthProbeServer({
-    bindAddress: healthcheckServer.bindAddress,
-    port: healthcheckServer.port,
-});
-
-function updateBootstrapList() {
-    const { replication } = config.extensions;
-    replication.destination.bootstrapList = config.getBootstrapList();
-
-    config.on('bootstrap-list-update', () => {
-        replication.destination.bootstrapList = config.getBootstrapList();
-    });
+function livenessCheck(res, log) {
+    if (bucketProcessor.isReady()) {
+        sendSuccess(res, log);
+    } else {
+        sendError(res, log, errors.ServiceUnavailable, 'unhealthy');
+    }
 }
 
-function startHealthProbe() {
-    healthServer.onReadyCheck(log => {
-        if (bucketProcessor.isReady()) {
-            return true;
+function probeServerSetup(config, done) {
+    startProbeServer(config, (err, probeServer) => {
+        if (err) {
+            return done(err);
         }
-        log.error('lifecycle bucket processor is not ready!');
-        return false;
-    });
-    logger.info('Starting HealthProbe server');
-    healthServer.start();
-}
 
-function loadManagementDatabase(cb) {
-    return initManagement({
-        serviceName: 'lifecycle',
-        serviceAccount: extensions.lifecycle.auth.account,
-        applyBucketWorkflows: applyBucketLifecycleWorkflows,
-    }, error => {
-        if (error) {
-            logger.error('could not load management db', { error });
-            setTimeout(loadManagementDatabase, 5000);
-            return;
+        if (!probeServer) {
+            logger.info('Skipping lifecycle bucket processor server setup');
+            return done();
         }
-        logger.info('management init done');
-        cb();
+
+        probeServer.addHandler(DEFAULT_LIVE_ROUTE, livenessCheck);
+        probeServer.addHandler(DEFAULT_READY_ROUTE, livenessCheck);
+        logger.info('Starting lifecycle bucket processor server');
+        return done();
     });
 }
 
-loadManagementDatabase(() => {
-    // Error is always caught and retried until the database is loaded.
-    updateBootstrapList();
-    startHealthProbe();
-    bucketProcessor.start();
+async.waterfall([
+    done => bucketProcessor.start(err => done(err)),
+    done => probeServerSetup(lcConfig.bucketProcessor.probeServer, done),
+], err => {
+    if (err) {
+        logger.error('error during lifecycle bucket processor initialization',
+            { error: err.message });
+        process.exit(1);
+    }
+    logger.info('lifecycle bucket processor running!');
 });
 
 process.on('SIGTERM', () => {
