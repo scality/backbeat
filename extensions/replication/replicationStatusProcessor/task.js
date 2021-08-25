553ca411c0576896c2a91c913207953e6c1083d9
Nicolas Humbert <nicolas.humbert@scality.com>
2021-08-25 17:33:23 -0400

    fix replication pods

--- extensions/replication/replicationStatusProcessor/task.js
+++ extensions/replication/replicationStatusProcessor/task.js
@@ -34,7 +34,7 @@ function initAndStart() {
             setTimeout(initAndStart, 5000);
             return;
         }
-        replicationStatusProcessor.start(null, startProbeServer(
+        replicationStatusProcessor.start({ bootstrap: false }, startProbeServer(
             repConfig.replicationStatusProcessor.probeServer,
             (err, probeServer) => {
                 if (err) {
