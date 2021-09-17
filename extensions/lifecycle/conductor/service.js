ffe9e3c0439818b782e2fe3021d040287e460b34
Naren <narendran.rajendran@scality.com>
2021-09-16 20:48:38 -0700

    improvement: BB-51 use probe server
    use the updated probe server in lifecycle conductor.

--- extensions/lifecycle/conductor/service.js
+++ extensions/lifecycle/conductor/service.js
@@ -1,44 +1,86 @@
 'use strict'; // eslint-disable-line
 
+const async = require('async');
 const werelogs = require('werelogs');
+const { errors } = require('arsenal');
+const {
+    DEFAULT_LIVE_ROUTE,
+    DEFAULT_METRICS_ROUTE,
+    DEFAULT_READY_ROUTE,
+} = require('arsenal').network.probe.ProbeServer;
+const { ZenkoMetrics } = require('arsenal').metrics;
 
 const LifecycleConductor = require('./LifecycleConductor');
-const { HealthProbeServer } = require('arsenal').network.probe;
-
 const config = require('../../../lib/Config');
+const { sendSuccess, sendError, startProbeServer } = require('../../../lib/util/probe');
+
 const zkConfig = config.zookeeper;
 const kafkaConfig = config.kafka;
 const lcConfig = config.extensions.lifecycle;
 const repConfig = config.extensions.replication;
 
-const lcConductor = new LifecycleConductor(
-    zkConfig, kafkaConfig, lcConfig, repConfig);
 
-werelogs.configure({ level: config.log.logLevel,
-                     dump: config.log.dumpLevel });
-const logger = new werelogs.Logger('Backbeat:Lifecycle:Conductor:service');
-const healthServer = new HealthProbeServer({
-    bindAddress: config.healthcheckServer.bindAddress,
-    port: config.healthcheckServer.port,
+const lcConductor = new LifecycleConductor(zkConfig, kafkaConfig, lcConfig, repConfig);
+
+werelogs.configure({
+    level: config.log.logLevel,
+    dump: config.log.dumpLevel,
 });
+const logger = new werelogs.Logger('Backbeat:Lifecycle:Conductor:service');
 
-lcConductor.start(err => {
+/**
+ * Handle ProbeServer liveness check
+ *
+ * @param {http.HTTPServerResponse} res - HTTP Response to respond with
+ * @param {Logger} log - Logger
+ * @returns {string} response
+ */
+function handleLiveness(res, log) {
+    if (lcConductor.isReady()) {
+        sendSuccess(res, log);
+    } else {
+        sendError(res, log, errors.ServiceUnavailable, 'unhealthy');
+    }
+}
+
+/**
+ * Handle ProbeServer metrics
+ *
+ * @param {http.HTTPServerResponse} res - HTTP Response to respond with
+ * @param {Logger} log - Logger
+ * @returns {string} response
+ */
+function handleMetrics(res, log) {
+    log.debug('metrics requested');
+    res.writeHead(200, {
+        'Content-Type': ZenkoMetrics.asPrometheusContentType(),
+    });
+    res.end(ZenkoMetrics.asPrometheus());
+}
+
+async.waterfall([
+    done => startProbeServer(config.healthcheckServer, (err, probeServer) => {
+        if (err) {
+            logger.error('error starting probe server', { error: err });
+            return done(err);
+        }
+        if (probeServer !== undefined) {
+            // following the same pattern as other extensions, where liveness
+            // and readiness are handled by the same handler
+            probeServer.addHandler([DEFAULT_LIVE_ROUTE, DEFAULT_READY_ROUTE], handleLiveness);
+            // retaining the old route and adding support to new route, until
+            // metrics handling is consolidated
+            probeServer.addHandler(['/_/monitoring/metrics', DEFAULT_METRICS_ROUTE], handleMetrics);
+        }
+        return done();
+    }),
+    done => lcConductor.start(done),
+], err => {
     if (err) {
-        logger.error('error during lifecycle conductor initialization',
-                     { error: err.message });
+        logger.error('error during lifecycle conductor initialization', { error: err.message });
         process.exit(1);
     }
-    healthServer.onReadyCheck(log => {
-        if (lcConductor.isReady()) {
-            return true;
-        }
-        log.error('lifecycle conductor is not ready!');
-        return false;
-    });
-    logger.info('Starting HealthProbe server');
-    healthServer.start();
     logger.info('lifecycle conductor process is running');
-    return undefined;
 });
 
 process.on('SIGTERM', () => {
