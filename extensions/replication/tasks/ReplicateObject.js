b8e95dfa48cd3a829a542c26b6df1dfcfd4e65b2
Artem Bakalov <artem.bakalov@scality.com>
2021-10-28 10:41:23 -0700

    resolve path

--- extensions/replication/tasks/ReplicateObject.js
+++ extensions/replication/tasks/ReplicateObject.js
@@ -21,7 +21,7 @@ const {
     metricsTypeCompleted,
     replicationStages
 } = require('../constants');
-const ObjectQueueEntry = require('../utils/ObjectQueueEntry');
+const ObjectQueueEntry = require('../utils/models/ObjectQueueEntry');
 
 const errorAlreadyCompleted = {};
 
