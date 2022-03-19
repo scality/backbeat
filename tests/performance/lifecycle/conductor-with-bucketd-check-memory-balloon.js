495906e3a7c9ad9828567a94aac4f5378d271743
Rached Ben Mustapha <rached.ben-mustapha@scality.com>
2022-03-19 02:31:16 +0000

    BB-142: throttle conductor depending on bucket queue

---
+++ tests/performance/lifecycle/conductor-with-bucketd-check-memory-balloon.js
@@ -0,0 +1,41 @@
+const baseConfig = require('../../../conf/Config');
+const LifecycleConductor = require('../../../extensions/lifecycle/conductor/LifecycleConductor');
+
+const lcConfig = {
+    ...baseConfig.extensions.lifecycle,
+    auth: {
+        type: '',
+    },
+    conductor: {
+        cronRule: '12 12 12 12 12',
+        concurrency: 10000,
+        bucketSource: 'bucketd',
+        bucketd: {
+            host: '127.0.0.1',
+            port: 9001,
+        },
+        backlogControl: {
+            enabled: true,
+        },
+    },
+};
+
+const lc = new LifecycleConductor(
+    baseConfig.zookeeper,
+    baseConfig.kafka,
+    lcConfig,
+    baseConfig.extensions.replication
+);
+
+describe('Lifecycle Conductor', function testBackpressure() {
+    this.timeout(10 * 60 * 1000);
+
+    it('should apply backpressure on bucket queue instead of ballooning', done => {
+        lc.init(() => {
+            lc.processBuckets(err => {
+                lc.stop();
+                done(err);
+            });
+        });
+    });
+});
