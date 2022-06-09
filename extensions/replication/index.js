3774d3b1762ca9aff8aced77f3a03ecde8b1b4fb
Kerkesni <walid.elansari@scality.com>
2022-06-09 17:41:05 +0200

    feature: BB-194 add OplogPopulator replication helper class

--- extensions/replication/index.js
+++ extensions/replication/index.js
@@ -1,8 +1,10 @@
 const ReplicationConfigValidator = require('./ReplicationConfigValidator');
+const ReplicationOplogPopulatorUtils = require('./ReplicationOplogPopulatorUtils');
 
 module.exports = {
     name: 'replication',
     version: '1.0.0',
     configValidator: ReplicationConfigValidator,
     queuePopulatorExtension: () => require('./ReplicationQueuePopulator'),
+    oplogPopulatorUtils: ReplicationOplogPopulatorUtils,
 };
