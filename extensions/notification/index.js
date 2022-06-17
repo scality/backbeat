49c49632cf822507194c47db2335634a6d28bbb9
Kerkesni <walid.elansari@scality.com>
2022-06-17 14:05:00 +0200

    feature: BB-205 fix notification queuePopulatorExtension export

--- extensions/notification/index.js
+++ extensions/notification/index.js
@@ -1,11 +1,10 @@
 const NotificationConfigValidator = require('./NotificationConfigValidator');
-const NotificationQueuePopulator = require('./NotificationQueuePopulator');
 const NotificationOplogPopulatorUtils = require('./NotificationOplogPopulatorUtils');
 
 module.exports = {
     name: 'notification',
     version: '1.0.0',
     configValidator: NotificationConfigValidator,
-    queuePopulatorExtension: NotificationQueuePopulator,
+    queuePopulatorExtension: () => require('./NotificationQueuePopulator'),
     oplogPopulatorUtils: NotificationOplogPopulatorUtils,
 };
