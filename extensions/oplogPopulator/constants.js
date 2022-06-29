5fa53df7e670e446c0ed82a55decf938fc798a60
Kerkesni <walid.elansari@scality.com>
2022-06-29 13:46:02 +0200

    feature: BB-219 configure kafka-connect mongo connector output message key
    Configure Kafka-connect MongoDB connector to only include bucket name in the Kafka message key.
    This will make events of the same bucket always land in the same partition as they will have the same key
    
    Having buckets always land in the same kafka partition will help spread the load over the consumers in a
    consumer group, as each consumer will only process specific buckets that were assigned to its partition.
    
    In the case where we cache bucket data, this is the case in the notification extension, consumers will only
    cache buckets assigned to them helping reduce memory usage

--- extensions/oplogPopulator/constants.js
+++ extensions/oplogPopulator/constants.js
@@ -6,6 +6,43 @@ const constants = {
         'change.stream.full.document': 'updateLookup',
         'pipeline': '[]',
         'collection': '',
+        // Kafka message key config
+        // The message key is set to only contain the bucket where the event happend.
+        // This will make events of the same bucket always land in the same partition
+        // as they will have the same key
+        'output.format.key': 'schema',
+        'output.schema.key': JSON.stringify({
+            type: 'record',
+            name: 'keySchema',
+            fields: [{
+                name: 'ns',
+                type: [{
+                        name: 'ns',
+                        type: 'record',
+                        fields: [{
+                            name: 'coll',
+                            type: ['string', 'null'],
+                        }],
+                    }, 'null'],
+            }, {
+                name: 'fullDocument',
+                type: [{
+                   type: 'record',
+                   name: 'fullDocumentRecord',
+                   fields: [{
+                        name: 'value',
+                        type: [{
+                            type: 'record',
+                            name: 'valueRecord',
+                            fields: [{
+                                name: 'key',
+                                type: ['string', 'null'],
+                            }],
+                        }, 'null'],
+                   }],
+                }, 'null'],
+            }],
+        }),
     },
     extensionConfigField: {
         notification: 'notificationConfiguration',
