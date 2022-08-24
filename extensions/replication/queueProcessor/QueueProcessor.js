de01e8f00c7484e930066e7d4ce4499f2bd9fcc4
Francois Ferrand <francois.ferrand@scality.com>
2022-08-24 16:51:37 +0200

    Use separate serviceName for ReplayProcessor
    Issue: BB-266

--- extensions/replication/queueProcessor/QueueProcessor.js
+++ extensions/replication/queueProcessor/QueueProcessor.js
@@ -231,7 +231,9 @@ class QueueProcessor extends EventEmitter {
         this._mProducer = null;
         this.site = site;
         this.mConfig = mConfig;
-        this.serviceName = libConstants.services.replicationQueueProcessor;
+        this.serviceName = this.isReplayTopic ?
+            libConstants.services.replicationReplayProcessor :
+            libConstants.services.replicationQueueProcessor;
         this.echoMode = false;
         this.scheduledResume = null;
         // bucket notification related
