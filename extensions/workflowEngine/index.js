4abc815ad339b70fee80f0a07ecaa2333094da9c
Francois Ferrand <francois.ferrand@scality.com>
2022-10-06 14:43:00 +0200

    Remove workflow-engine
    Issue: BB-303

--- extensions/workflowEngine/index.js
+++
@@ -1,8 +0,0 @@
-const WorkflowEngineConfigValidator =
-      require('./WorkflowEngineConfigValidator');
-
-module.exports = {
-    name: 'workflow-engine',
-    version: '1.0.0',
-    configValidator: WorkflowEngineConfigValidator,
-};
--- extensions/workflowEngineDataSource/index.js
+++
@@ -1,9 +0,0 @@
-const WorkflowEngineDataSourceConfigValidator =
-      require('./WorkflowEngineDataSourceConfigValidator');
-
-module.exports = {
-    name: 'workflow-engine-data-source',
-    version: '1.0.0',
-    configValidator: WorkflowEngineDataSourceConfigValidator,
-    queuePopulatorExtension: () => require('./WorkflowEngineDataSourceQueuePopulator'),
-};
