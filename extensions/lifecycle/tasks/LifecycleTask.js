17dc701a9e8c440a1dbe945a2e23629d022a1d32
Alexander Chan <alexander.chan@scality.com>
2023-04-14 00:13:37 -0700

    BB-383: add missing skip case for latest version transitions

--- extensions/lifecycle/tasks/LifecycleTask.js
+++ extensions/lifecycle/tasks/LifecycleTask.js
@@ -1149,7 +1149,7 @@ class LifecycleTask extends BackbeatTask {
         if (objectInfo.Size === 0
             && locationsConfig[site]
             && locationsConfig[site].type === 'dmf') {
-            log.debug(`skipping transition task for 0 bytes objects to DMF location`, {
+            log.debug('skipping transition task for 0 bytes objects to DMF location', {
                 site,
             });
             return true;
@@ -1500,7 +1500,8 @@ class LifecycleTask extends BackbeatTask {
                 log);
             return done();
         }
-        if (rules.Transition) {
+        if (rules.Transition &&
+            !this._bb383SkipDMFTransition(version, rules.Transition.StorageClass, log)) {
             this._applyTransitionRule({
                 owner: bucketData.target.owner,
                 accountId: bucketData.target.accountId,
