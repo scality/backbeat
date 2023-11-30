75532c802c59ccf7b979412cf64225a090c71dd8
Nicolas Humbert <nicolas.humbert@scality.com>
2023-11-30 15:10:43 +0100

    BB-473 Probe response logic should be handled in the handler
    Currently, the probe response logic is distributed between Backbeat probe handlers and Arsenal's onRequest method.
    This scattered approach causes confusion for developers and results in bugs.
    The solution is to centralize the probe response logic exclusively within the Backbeat probe handlers.

--- tests/unit/replication/ReplicationStatusProcessor.js
+++ tests/unit/replication/ReplicationStatusProcessor.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const promClient = require('prom-client');
+const sinon = require('sinon');
 
+const constants = require('../../../lib/constants');
 const ReplicationStatusProcessor =
     require('../../../extensions/replication/replicationStatusProcessor/ReplicationStatusProcessor');
 
@@ -135,6 +137,143 @@ describe('ReplicationStatusProcessor', () => {
                 'backbeat-replication-replay-0',
                 'backbeat-replication-replay-1',
             ]);
+        });
+    });
+});
+
+describe('ReplicationStatusProcessor: handlers', () => {
+    let processor;
+    let mockResponse;
+    let logger;
+    const topicName = 'backbeat-replication-replay-0';
+    const replayTopics = [
+        {
+            topicName,
+            retries: 5,
+        },
+    ];
+
+    beforeEach(() => {
+        // Clear register to avoid:
+        // Error: A metric with the name kafka_lag has already been registered.
+        promClient.register.clear();
+
+        processor = makeReplicationStatusProcessor(replayTopics);
+        mockResponse = {
+            writeHead: sinon.stub(),
+            end: sinon.stub(),
+        };
+        logger = {
+            debug: sinon.stub(),
+            error: sinon.stub(),
+        };
+
+        sinon.stub(promClient.register, 'metrics').resolves('metrics_data');
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe('handleLiveness', () => {
+        it('should respond with 200 when all components are healthy', () => {
+            processor._consumer = { _consumerReady: true };
+            processor._FailedCRRProducer = { _producer: { isReady: () => true } };
+            processor._ReplayProducers = {
+                [topicName]: { _producer: { isReady: () => true } },
+            };
+
+            processor.handleLiveness(mockResponse, logger);
+
+            assert(mockResponse.writeHead.calledWith(200));
+            assert(mockResponse.end.calledOnce);
+        });
+
+        it('should respond with 500 when consumer is not ready', () => {
+            processor._consumer = { _consumerReady: false };
+            processor._FailedCRRProducer = { _producer: { isReady: () => true } };
+            processor._ReplayProducers = {
+                [topicName]: { _producer: { isReady: () => true } },
+            };
+
+            processor.handleLiveness(mockResponse, logger);
+
+            sinon.assert.calledOnceWithExactly(mockResponse.writeHead, 500);
+
+            const expectedRes = [{
+                component: 'Consumer',
+                status: constants.statusNotReady,
+            }];
+            sinon.assert.calledOnceWithExactly(mockResponse.end, JSON.stringify(expectedRes));
+
+            const expectedLogError = {
+                httpCode: 500,
+                error: expectedRes,
+            };
+            sinon.assert.calledOnceWithExactly(logger.error, 'sending back error response', expectedLogError);
+        });
+
+        it('should respond with 500 when failed CRR Producer is not ready', () => {
+            processor._consumer = { _consumerReady: true };
+            processor._FailedCRRProducer = { _producer: { isReady: () => false } };
+            processor._ReplayProducers = {
+                [topicName]: { _producer: { isReady: () => true } },
+            };
+
+            processor.handleLiveness(mockResponse, logger);
+
+            sinon.assert.calledOnceWithExactly(mockResponse.writeHead, 500);
+
+            const expectedRes = [{
+                component: 'Failed CRR Producer',
+                status: constants.statusNotReady,
+            }];
+            sinon.assert.calledOnceWithExactly(mockResponse.end, JSON.stringify(expectedRes));
+
+            const expectedLogError = {
+                httpCode: 500,
+                error: expectedRes,
+            };
+            sinon.assert.calledOnceWithExactly(logger.error, 'sending back error response', expectedLogError);
+        });
+
+        it('should respond with 500 when Replay Producers is not ready', () => {
+            processor._consumer = { _consumerReady: true };
+            processor._FailedCRRProducer = { _producer: { isReady: () => true } };
+            processor._ReplayProducers = {
+                [topicName]: { _producer: { isReady: () => false } },
+            };
+
+            processor.handleLiveness(mockResponse, logger);
+
+            sinon.assert.calledOnceWithExactly(mockResponse.writeHead, 500);
+
+            const expectedRes = [{
+                component: 'Replay CRR Producer to backbeat-replication-replay-0',
+                status: constants.statusNotReady,
+            }];
+            sinon.assert.calledOnceWithExactly(mockResponse.end, JSON.stringify(expectedRes));
+
+            const expectedLogError = {
+                httpCode: 500,
+                error: expectedRes,
+            };
+            sinon.assert.calledOnceWithExactly(logger.error, 'sending back error response', expectedLogError);
+        });
+    });
+
+    describe('handleMetrics', () => {
+        it('should properly handle metrics response', async () => {
+            const r = await processor.handleMetrics(mockResponse, logger);
+            assert.strictEqual(r, undefined);
+
+            sinon.assert.calledOnce(promClient.register.metrics);
+
+            sinon.assert.calledOnceWithExactly(mockResponse.writeHead, 200, {
+                'Content-Type': promClient.register.contentType,
+            });
+
+            sinon.assert.calledOnceWithExactly(mockResponse.end, 'metrics_data');
         });
     });
 });
