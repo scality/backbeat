039395820768d42c48263df1afb6a1fadae11935
KillianG <killian.gardahaut@scality.com>
2024-02-20 11:44:56 +0100

    Adding owner id to garbage collector message
    Issue: BB-503

--- extensions/lifecycle/tasks/LifecycleUpdateExpirationTask.js
+++ extensions/lifecycle/tasks/LifecycleUpdateExpirationTask.js
@@ -113,7 +113,7 @@ class LifecycleUpdateExpirationTask extends BackbeatTask {
     }
 
     _garbageCollectLocation(entry, locations, log, done) {
-        const { bucket, key, version, eTag, accountId } = entry.getAttribute('target');
+        const { bucket, key, version, eTag, accountId, ownerId } = entry.getAttribute('target');
         const gcEntry = ActionQueueEntry.create('deleteData')
               .addContext({
                   origin: 'lifecycle',
@@ -127,7 +127,9 @@ class LifecycleUpdateExpirationTask extends BackbeatTask {
               .setAttribute('source', entry.getAttribute('source'))
               .setAttribute('serviceName', 'lifecycle-transition')
               .setAttribute('target.locations', locations)
-              .setAttribute('target.accountId', accountId);
+              .setAttribute('target.accountId', accountId)
+              .setAttribute('target.ownerId', ownerId);
+
         this.gcProducer.publishActionEntry(gcEntry);
         return process.nextTick(done);
     }
