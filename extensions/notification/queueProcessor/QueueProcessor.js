5a8263567908deb68a1806709310bb8202a38df8
KillianG <Killian.gardahaut@scality.com>
2024-11-07 17:09:48 +0100

    Add default labels for Kafka lag metrics in QueueProcessor

--- extensions/notification/queueProcessor/QueueProcessor.js
+++ extensions/notification/queueProcessor/QueueProcessor.js
@@ -6,6 +6,7 @@ const async = require('async');
 const assert = require('assert');
 const util = require('util');
 const { ZenkoMetrics } = require('arsenal').metrics;
+const { wrapGaugeSet } = require('../../../lib/util/metrics')
 const errors = require('arsenal').errors;
 
 const BackbeatConsumer = require('../../../lib/BackbeatConsumer');
@@ -33,6 +34,9 @@ const kafkaLagMetric = ZenkoMetrics.createGauge({
     labelNames: ['origin', 'containerName', 'partition', 'serviceName'],
 });
 
+const defaultLabels = {
+    origin: 'notification',
+};
 
 /**
  * Contains methods to incrememt different metrics
