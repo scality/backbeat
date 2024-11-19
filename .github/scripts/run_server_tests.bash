68e38e34ceb6389a5c4b26236ff3b2f5fc9504c7
KillianG <Killian.gardahaut@scality.com>
2024-11-19 17:02:04 +0100

    remove --silent

--- .github/scripts/run_server_tests.bash
+++ .github/scripts/run_server_tests.bash
@@ -11,7 +11,7 @@ killandsleep () {
   sleep 10
 }
 
-./node_modules/.bin/nyc --clean --silent yarn start 2>&1 &
+./node_modules/.bin/nyc --clean yarn start &
 bash tests/utils/wait_for_local_port.bash $PORT 120
 yarn run $1
 killandsleep
