6f0c538f5170cff3242db796393737fb82849a4e
Maha Benzekri <maha.benzekri@scality.com>
2024-12-31 16:35:01 +0100

    fixup tests

--- tests/functional/lifecycle/LifecycleTask.js
+++ tests/functional/lifecycle/LifecycleTask.js
@@ -63,12 +63,15 @@ const backbeatMetadataProxyMock = {
 // current date
 const CURRENT = new Date();
 CURRENT.setDate(CURRENT.getDate() - 2);
+CURRENT.setUTCHours(0, 0, 0, 0);
 // 5 days prior to currentDate
 const PAST = new Date(CURRENT);
 PAST.setDate(PAST.getDate() - 5);
+PAST.setUTCHours(0, 0, 0, 0);
 // 5 days after currentDate
 const FUTURE = new Date(CURRENT);
 FUTURE.setDate(FUTURE.getDate() + 5);
+FUTURE.setUTCHours(0, 0, 0, 0); 
 
 const OWNER = 'testOwner';
 const ncd = 'NoncurrentDays';
