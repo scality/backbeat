994f980fc76a5e628188cea10529058b84f58de3
Kerkesni <walid.elansari@scality.com>
2025-02-21 15:54:08 +0100

    fix OOB delete entries always expecting overhead fields
    Overhead fields for delete events are supported on recent
    versions of S3C.
    
    Issue: BB-656

--- tests/unit/lib/models/ObjectQueueEntry.spec.js
+++ tests/unit/lib/models/ObjectQueueEntry.spec.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 const QueueEntry = require('../../../../lib/models/QueueEntry');
 const { replicationEntry } = require('../../../utils/kafkaEntries');
+const DeleteOpQueueEntry = require('../../../../lib/models/DeleteOpQueueEntry');
 
 describe('ObjectQueueEntry', () => {
     describe('toRetryEntry', () => {
@@ -17,6 +18,34 @@ describe('ObjectQueueEntry', () => {
             const retryEntry = entry.toRetryEntry('sf');
             assert.strictEqual(retryEntry.getReplicationBackends().length, 1);
             assert.strictEqual(retryEntry.getReplicationBackends()[0].site, 'sf');
+        });
+    });
+    describe('createFromKafkaEntry', () => {
+        it('should create a DeleteOpQueueEntry without overhead fields', () => {
+            const entry = QueueEntry.createFromKafkaEntry({
+                value: JSON.stringify({
+                    type: 'del',
+                    bucket: 'bucket',
+                    key: 'key',
+                }),
+            });
+            assert(entry instanceof DeleteOpQueueEntry);
+            assert.strictEqual(entry.getBucket(), 'bucket');
+            assert.strictEqual(entry.getKey(), 'key');
+        });
+        it('should create a DeleteOpQueueEntry with overhead fields', () => {
+            const entry = QueueEntry.createFromKafkaEntry({
+                value: JSON.stringify({
+                    type: 'del',
+                    bucket: 'bucket',
+                    key: 'key',
+                    overheadFields: JSON.stringify({ foo: 'bar' }),
+                }),
+            });
+            assert(entry instanceof DeleteOpQueueEntry);
+            assert.strictEqual(entry.getBucket(), 'bucket');
+            assert.strictEqual(entry.getKey(), 'key');
+            assert.deepStrictEqual(entry.getOverheadField('foo'), 'bar');
         });
     });
 });
